3a6d15f639af8a8fe1a0bc3db45789567fd486c2
Vladimir Shcherbakov <vlashch@microsoft.com>
2018-09-30 18:16:08 -0700

    first commit

--- main.m
+++ main.m
@@ -7,7 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "ComputerVisionClient.h"
+#import "ComputerVision/ComputerVisionClient.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
